17af684ad08a3fda80c54b2ddbf7e4c1853bfc1f
k2 <kazzameer@me.com>
2017-05-24 17:15:59 +0700

    Add main wireframe

---
+++ Assets/Scripts/AppContext.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable 0414
+
+using UnityEngine;
+using System.Collections;
+
+using strange.extensions.command.api;
+using strange.extensions.command.impl;
+using strange.extensions.context.impl;
+using strange.extensions.context.api;
+using strange.extensions.signal.api;
+using strange.extensions.signal.impl;
+
+namespace App
+{
+    public class AppContext : MVCSContext {
+		private Main _main = null;
+
+        public AppContext(Main main): base(main, true)
+        {
+            _main = main;
+        }
+	}
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using strange.extensions.context.impl;
+
+namespace App
+{
+	public class Main : ContextView {
+		void Awake()
+		{
+			Random.InitState(128);
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
+			context = new AppContext(this);
+			context.Start();
+		}		
+	}
+}
