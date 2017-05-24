c81f6ad780811c1ec94cf0136fde9517fd356020
k2 <kazzameer@me.com>
2017-05-25 00:21:10 +0700

    Add naive level generation
    + All the walls must be 1x1 in size
    + It keeps minimum 1 hole per row

---
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using App.Models;
+
+namespace App.Level
+{
+	public class LevelGenerator {
+		[Inject]
+		public LevelGeneratorModel LevelGeneratorModel { get; set; }
+		
+		private const int MAX_ROWS = 8;
+		private const int MAX_COLUMNS = 3;
+
+		private T GetRandom<T>(List<T> list) {
+			return list[Random.Range(0, list.Count)];
+		}
+
+		public LevelSegment GenerateSegment(LevelSegment previousSegment = null)
+		{
+			var obstacles = LevelGeneratorModel.Obstacles;
+			var floors = LevelGeneratorModel.Floors;
+
+			var floor = GetRandom(floors);
+			LevelSegment segment = new LevelSegment(floor.Prefab);
+			
+			int currentRow = 0;
+			int currentColumn = 0;
+
+			if (previousSegment == null) {
+				// need to reserve {1, 0} point as player start
+				segment.Set(1, 0, SegmentValue.PlayerStart);
+				currentRow = 1;
+			}
+
+			for (; currentRow < MAX_ROWS; currentRow += 2)
+			{
+				for (currentColumn = 0; currentColumn < MAX_COLUMNS; currentColumn++)
+				{
+					if (Random.Range(.0f, 10.0f) <= 5.0f) {
+						if (segment.FreeCellsInRow(currentRow) > 1) {
+							segment.Set(currentColumn, currentRow, SegmentValue.Wall);
+						}
+					}
+				}
+			}
+			
+			return segment;
+		}
+	}
+}
---
+++ Assets/Scripts/Level/LevelSegment.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Level
+{
+	public enum SegmentValue {
+		Empty = 0,
+		Wall,
+		PlayerStart
+	}
+	public class LevelSegment 
+	{
+		private string _floorPrefab;
+		private SegmentValue[,] _plan = new SegmentValue[3, 8];
+		public LevelSegment(string floorPrefab)
+		{
+			_floorPrefab = floorPrefab;
+		}
+
+		public int FreeCellsInRow(int rowIndex) {
+			SegmentValue[] row = {_plan[0, rowIndex], _plan[1, rowIndex], _plan[2, rowIndex]};
+			int freeCells = 0;
+			for (int i = 0; i < row.Length; ++i) {
+				if (row[i] == SegmentValue.Empty)
+				{
+					freeCells++;	
+				}
+			}
+			return freeCells;
+		}
+		public SegmentValue Get(int x, int y)
+		{
+			return _plan[x, y];
+		}
+
+		public void Set(int x, int y, SegmentValue cell)
+		{
+			_plan[x, y] = cell;
+		}
+	}
+}
