d0bb95af309d40f3b323fa1f7c2f29077399ff36
k2 <kazzameer@me.com>
2017-05-25 03:15:01 +0700

    Add move left/right signals

--- Assets/Scripts/Signals/CoreSignals.cs
+++ Assets/Scripts/Signals/CoreSignals.cs
@@ -4,4 +4,6 @@ using strange.extensions.signal.impl;
 namespace App.Signals
 {
 	public class InitiateSignal: Signal {}
+	public class MoveLeftSignal: Signal {}
+	public class MoveRightSignal: Signal {}
 }
