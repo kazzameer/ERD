3554bca139ed8e7c5a50b6fd952bcfc29679f906
k2 <kazzameer@me.com>
2017-05-25 01:44:47 +0700

    Add initial Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App
+{
+	public class Player : MonoBehaviour {
+		private Animator _animatorController = null;
+		void Awake()
+		{
+			_animatorController = gameObject.GetComponent<Animator>();
+			_animatorController.SetFloat("Forward", 0.5f);
+		}
+	}
+
+}
