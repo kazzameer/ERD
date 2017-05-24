051332080f4587986ea3c73114af4a166c12c24d
k2 <kazzameer@me.com>
2017-05-25 02:45:47 +0700

    Add anchor to attach camera to

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 namespace App
 {
 	public class Player : MonoBehaviour {
+		[SerializeField] Transform CameraAnchor = null;
 		private Animator _animatorController = null;
+
+		public Transform Anchor
+		{
+			get {
+				return CameraAnchor;
+			}
+		}
 		void Awake()
 		{
 			_animatorController = gameObject.GetComponent<Animator>();
-			_animatorController.SetFloat("Forward", 0.5f);
+			_animatorController.SetFloat("Forward", 0.0f);
 		}
 	}
 
