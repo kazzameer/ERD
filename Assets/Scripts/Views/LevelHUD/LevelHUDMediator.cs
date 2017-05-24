cfbf61b68637b6bb1b6b1c1dab92cf6cfd88b226
k2 <kazzameer@me.com>
2017-05-25 03:16:27 +0700

    Add game HUD
    + a prefab with view and mediator
    + processing left/right buttons clicks

---
+++ Assets/Scripts/Views/LevelHUD/LevelHUDMediator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using strange.extensions.mediation.impl;
+
+using App.Signals;
+
+namespace App.Views
+{
+	public class LevelHUDMediator : Mediator 
+	{
+		[Inject]
+		public LevelHUDView View { get; set; }
+
+		[Inject]
+		public MoveLeftSignal MoveLeft { get; set; }
+		[Inject]
+		public MoveRightSignal MoveRight { get; set; }
+
+		public override void OnRegister()
+		{
+			base.OnRegister();
+			View.OnLeft.AddListener(OnLeft);
+			View.OnRight.AddListener(OnRight);
+		}
+
+		private void OnLeft()
+		{
+			MoveLeft.Dispatch();
+		}
+
+		private void OnRight()
+		{
+			MoveRight.Dispatch();
+		}
+
+		public override void OnRemove()
+		{
+			base.OnRemove();
+			View.OnLeft.RemoveListener(OnLeft);
+			View.OnRight.RemoveListener(OnRight);
+		}
+	}
+}
---
+++ Assets/Scripts/Views/LevelHUD/LevelHUDView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using strange.extensions.mediation.impl;
+using strange.extensions.signal.impl;
+
+namespace App.Views
+{
+	public class LevelHUDView : View 
+	{
+		public Signal OnLeft = new Signal();
+		public Signal OnRight = new Signal();
+
+		public void OnLeftClick()
+		{
+			OnLeft.Dispatch();
+		}
+
+		public void OnRightClick() 
+		{
+			OnRight.Dispatch();
+		}
+	}
+}
