86d08f6e18e070ed202014a9c619f594d88c7f32
k2 <kazzameer@me.com>
2017-05-30 01:27:36 +0700

    Generate 3 floor instances
    + keep them generating as player advances further
    + Count segments passed

--- Assets/Scripts/Views/Level/LevelView.cs
+++ Assets/Scripts/Views/Level/LevelView.cs
@@ -23,6 +23,15 @@ namespace App.Views
 		private int _currentTrack = 1;
 		private GameObject _currentSegment = null;
 		private bool _playerSteering = false;
+		private int _segmentCounter = 0;
+		private float _segmentProgress = .0f;
+		private List<GameObject> _segments = new List<GameObject>();
+
+		private bool _pendNewSegment = false;
+		private int _lastSegment = 0;
+		private bool _removedLastTile = false;
+
+
 		private Dictionary<int, float> _cellOffset = new Dictionary<int, float> {
 			{0, -1.0f},
 			{1, .0f},
@@ -30,6 +39,8 @@ namespace App.Views
 		};
 		public void GenerateInitialSegment()
 		{
+			BuildSegment(LevelGenerator.GenerateSegment());
+			BuildSegment(LevelGenerator.GenerateSegment());
 			BuildSegment(LevelGenerator.GenerateSegment());
 		}
 		
@@ -64,10 +75,11 @@ namespace App.Views
 			controller.SetTarget(_player.Anchor);
 		}
 
-		private GameObject BuildSegment(LevelSegment segment)
+	private void BuildSegment(LevelSegment segment)
 		{
 			GameObject segmentInstance = new GameObject();
 			segmentInstance.transform.SetParent(this.transform, false);
+			segmentInstance.transform.position = new Vector3(0, 0, _segmentCounter * 8.0f);
 			var obstacles = LevelGeneratorModel.Obstacles;
 
 			var floorInst = Instantiate<GameObject>(LevelGeneratorModel[segment.FloorPrefab]);
@@ -85,18 +97,18 @@ namespace App.Views
 							var obstaclePrefab = LevelGeneratorModel[obstacle.Prefab];
 							var inst = Instantiate<GameObject>(obstaclePrefab);
 							inst.transform.SetParent(segmentInstance.transform, false);
-							inst.transform.position = new Vector3(_cellOffset[i], 0, j);	
+							inst.transform.localPosition = new Vector3(_cellOffset[i], 0, j);	
 						} break;
 
 						case SegmentValue.PlayerStart: {
-							_player.transform.position = new Vector3(_cellOffset[i], 0, j);
+//							_player.transform.position = new Vector3(_cellOffset[i], 0, j);
 						} break;
 					}
 				}
 			}
 			
-
-			return segmentInstance;
+			_segmentCounter++;
+			_segments.Add(segmentInstance);
 		}
 
 		public void SpawnPlayer()
@@ -108,6 +120,38 @@ namespace App.Views
 				_player = playerInstance.GetComponent<Player>();
 			}
 			
+		}
+		
+		void LateUpdate()
+		{
+			if (_player != null)
+			{
+				var advance = _player.gameObject.transform.position.z;
+				int segmentNumber = (int)(advance / 8.0f);
+
+				if (segmentNumber != _lastSegment) {
+					_lastSegment = segmentNumber;
+					_pendNewSegment = false;
+					_removedLastTile = false;
+				}
+
+				_segmentProgress = (advance - segmentNumber * 8.0f ) / 8.0f;
+
+				if (_segmentProgress > 0.95f && !_pendNewSegment) {
+					_pendNewSegment = true;
+					BuildSegment(LevelGenerator.GenerateSegment());
+				}
+
+				if (_segmentProgress > 0.25 && !_removedLastTile && segmentNumber > 0) {
+					_removedLastTile = true;
+					PopSegment();
+				}
+			}
+		}
+		private void PopSegment()
+		{
+			Destroy(_segments[0].gameObject);
+			_segments.RemoveAt(0);
 		}
 	}
 }
