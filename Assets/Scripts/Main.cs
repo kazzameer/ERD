9d3db9de64c6dddce640c2357ee7ded776059a69
k2 <kazzameer@me.com>
2017-06-04 18:09:48 +0700

    Add sound processing

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -8,12 +8,30 @@ namespace App
 	public class Main : ContextView {
 		[SerializeField] Transform Root;
 		[SerializeField] Transform UI;
+		[SerializeField] AudioClip CoinSound = null;
+		private AudioSource _audioSource = null;
+
+		private static Main __instance = null;
+
+		public static Main Instance {
+			get {
+				return __instance;
+			}
+		}
+
 		void Awake()
 		{
+			__instance = this;
 			//Random.InitState(128);
 			Screen.sleepTimeout = SleepTimeout.NeverSleep;
 			context = new AppContext(this);
 			context.Start();
+			_audioSource = gameObject.GetComponent<AudioSource>();
+
+		}
+
+		public void PlayCoinCollectSound() {
+			_audioSource.PlayOneShot(CoinSound);
 		}
 
 		public Transform World {
